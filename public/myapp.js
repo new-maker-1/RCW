fbf069ce393994c9b00da3bf748587040a7bd864
new-maker-1 <new-maker-1@outlook.com>
2017-06-24 23:27:33 +0800

    angular

---
+++ public/myapp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 6/24/2017.
+ */
+
+var myapp = angular.module('myApp', []);
+
+myapp.controller('myheader', function($scope){
+    $scope.mytitle = "MY HEADER !";
+});
